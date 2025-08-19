58eb562e04043714dc7d97da1e8979745707f188
Waisman Dourado <waismandourado@gmail.com>
2025-08-18 21:29:16 -0300

    Update schemas (user) into schemas.py file

--- app/schemas.py
+++ app/schemas.py
@@ -2,18 +2,31 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 # ---Data Models for the Backend (CRUD, etc.)---
-class UserCreate(BaseModel):
-    username: str
+class UserBase(BaseModel):
     email: str
+
+class UserCreate(UserBase):
+    username: str
     password: str
 
-class User(BaseModel):
+class User(UserBase):
     id: int
     username: str
-    email: str
+    is_active = True
 
 class Config:
     orm_model = True # Enable ORM mapping for Pydantic
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
 
 # New Model for Credit Forecasting
 class CreditApplication(BaseModel):
